f88101d55e8b38b3c533c489c9a6c9ddf15db738
Oliver N <olvrng@gmail.com>
2021-01-23 11:59:44 +0700

    fix deploy

--- api/[name].ts
+++ api/[name].ts
@@ -1,4 +1,4 @@
-import { NowRequest, NowResponse } from '@vercel/node'
+import type { NowRequest, NowResponse } from '@vercel/node'
 
 export default (req: NowRequest, res: NowResponse) => {
   const {query} = req
