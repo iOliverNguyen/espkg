bceae59da7c96076744f26392496ec04ac513fe1
Oliver N <olvrng@gmail.com>
2021-01-23 11:26:35 +0700

    convert to snowpack

---
+++ snowpack.config.js
@@ -0,0 +1,15 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: {
+      url: "/",
+      static: true,
+    },
+    src: {
+      url: "/",
+    },
+  },
+  plugins: [
+    ['@snowpack/plugin-svelte'],
+  ]
+};
--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,7 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello from {name}!</h1>
 </main>
 
 <style>
@@ -17,7 +16,6 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
